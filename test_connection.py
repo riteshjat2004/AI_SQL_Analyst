a12f0d27f39a93337cc2e2ccf9ba358f6a9416a5
Ritesh Jat <link4riteshjat@gmail.com>
2026-07-23 01:35:08 +0530

    feat: implement PostgreSQL database connection

---
+++ test_connection.py
@@ -0,0 +1,20 @@
+from sqlalchemy import text
+from database.connection import engine
+
+with engine.connect() as connection:
+
+    result = connection.execute(
+        text("""
+        SELECT
+            product_name,
+            category,
+            price
+        FROM products
+        LIMIT 5;
+        """)
+    )
+
+    print("\nProducts\n")
+
+    for row in result:
+        print(row)
